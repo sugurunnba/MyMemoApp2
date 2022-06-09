c4ce125194ff809610ce8c5d05feec098a26f739
sugurunnba <sugurunnba7@gmail.com>
2022-06-09 23:04:59 +0900

    first commit

---
+++ MyMemoApp2/MemoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemoViewController.swift
+//  MyMemoApp2
+//
+//  Created by 高木克 on 2022/06/09.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
